f960a029ce8e92a640fe2c144bf380e525bc163c
champion <jphilippe.champion@gmail.com>
2023-07-13 19:12:12 +0200

    maj des extensions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,16 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
+class HomeController extends AbstractController
 {
-    #[Route('/')]
-    public function number() : Response
-    {
-	return new Response(rand(0,100));
-     }
+  #[Route('/')]
+  public function number() : Response
+  {
+	$number = rand(0, 100);
+	return $this->render('base.html.twig', [
+    	  	'number' => $number,
+	]);
+  }
 }
