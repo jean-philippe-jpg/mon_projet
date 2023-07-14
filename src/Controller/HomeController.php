b83d943d4efdcc8298c43ae00f26334e4a66df93
champion <jphilippe.champion@gmail.com>
2023-07-14 13:40:21 +0200

    integration header footer au dossier partials

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,18 @@
 <?php
+
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-  #[Route('/')]
-  public function number() : Response
-  {
-	$number = rand(0, 100);
-	return $this->render('base.html.twig', [
-    	  	'number' => $number,
-	]);
-  }
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
 }
