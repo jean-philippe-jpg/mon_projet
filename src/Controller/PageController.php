92ff2ce4f5f19c2439224b04d6df20932284c39f
champion <jphilippe.champion@gmail.com>
2023-07-14 15:58:04 +0200

    added page contact

---
+++ src/Controller/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/contact', name: 'page_contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+
+            'title' => 'contact'
+        ]);
+    }
+}
